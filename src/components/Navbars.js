f9c4aaadb93fc212a302066c9be219b90ca75d64
YW Z <32604270+echoztoronto@users.noreply.github.com>
2021-07-10 21:20:29 -0400

    removed unused package

--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -10,10 +10,9 @@ export default function Navbars() {
             <Navbar.Brand href="/"> Yuwen Zheng </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="container-fluid">
+                <Nav className="mr-auto">
                     <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="/project">Projects</Nav.Link>
-                    <Nav.Link href="https://github.com/echoztoronto" className="ml-auto"> GitHub</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
