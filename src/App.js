156cc0d645a3109eb110bf704fa7db3cd25af21b
YW Z <32604270+echoztoronto@users.noreply.github.com>
2021-07-12 03:33:16 -0400

    github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch }  from "react-router-dom";
+import { HashRouter, Route, Switch }  from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Project from "./components/Project";
@@ -6,14 +6,14 @@ import Navbars from "./components/Navbars";
 
 function App() {
   return (
-    <BrowserRouter >
+    <HashRouter basename={process.env.PUBLIC_URL + "/"}>
     <Navbars />
       <Switch>
-        <Route component={Home} path='/' exact />
         <Route component={About} path='/about' exact/>
         <Route component={Project} path='/project' exact/>
+        <Route component={Home} path='/' exact />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -7,12 +7,12 @@ import './../style.css'
 export default function Navbars() {
     return (
         <Navbar className="navbar bg-dark" variant="dark" expand="lg" fixed="top">
-            <Navbar.Brand href="/"> Yuwen (Echo) Zheng </Navbar.Brand>
+            <Navbar.Brand href="/portfolio"> Yuwen (Echo) Zheng </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/about">About</Nav.Link>
-                    <Nav.Link href="/project">Projects</Nav.Link>
+                    <Nav.Link href="/portfolio/#/about">About</Nav.Link>
+                    <Nav.Link href="/portfolio/#/project">Projects</Nav.Link>
                     <Nav.Link href="https://github.com/echoztoronto">GitHub</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
